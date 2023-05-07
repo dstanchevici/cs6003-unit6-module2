30faa78f7c296c1959f41c787417ec7c4ffb5d1e
dstanchevici <distanchev@gmail.com>
2023-05-07 16:06:24 -0400

    2.2

---
+++ StaticExample2.java
@@ -0,0 +1,26 @@
+public class StaticExample2 {
+
+    static double x;
+
+    static void printx ()
+    {
+	System.out.println ("x = " + x);
+    }
+
+    double y;
+
+    void printy ()
+    {
+	System.out.println ("y = " + y);
+    }
+
+    public static void main (String[] argv)
+    {
+	x = 5.34;
+	printx ();
+
+	y = 9.67;
+	printy ();
+    }
+
+}
